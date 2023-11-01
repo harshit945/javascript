ce8fb081eea2a266bd8895d1ede4f07f11a4730f
Harshit Chaturvedi <harshit.chaturvedi@juspay.in>
2023-11-01 15:29:38 +0530

    promise introduction

---
+++ Asynchronous JavaScript/1-promises.js
@@ -0,0 +1,6 @@
+// promises basically has three stages 
+// 1 - Pending : When you placed your order you have to wait for sometime
+// 2 - FullFill : When your order is ready
+// 3 - Rejected : When order is not fulfiled
+
+// Promise is considered to be settled if its in one of these two stages one is fullfill or rejected else its in pending stage
